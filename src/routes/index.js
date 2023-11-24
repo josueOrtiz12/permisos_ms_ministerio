e11a93a745507a80987daaaebd642ad2877288aa
Adolfo Chafardett <adolfo1997a@gmail.com>
2023-11-24 11:09:45 -0400

    feat: adding new module

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,5 +5,6 @@ const app = express()
 router.use('/users', require('../modules/users'))
 router.use('/roles', require('../modules/roles'))
 router.use('/resources', require('../modules/resources'))
+router.use('/roles/users', require('../modules/roles_by_users'))
 
 module.exports = router
